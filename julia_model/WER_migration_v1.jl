ce8449c6671af085a442aec60961601e5b68876a
Aurora Cossairt <a.cossairt@outlook.com>
2025-04-16 17:29:42 -0700

    create practice space for mtk simple

--- julia_model/WER_migration_v1.jl
+++ julia_model/WER_migration_v1.jl
@@ -14,12 +14,12 @@ D = Differential(t)
 #=Parameters. Note: must define paramater "t" before defining the state variables (thus defined above), otherwise julia will 
 complain about undefined parameter 't'. Time, t, can also be defined as a variable, but the distinction is not clear. These
 are put into named vectors for convenience. Note that the default parameter values are set in the declaration. This is not 
-required, it is just convenient. See comment below on vars=# 
+required, it is just convenient. See comment below on vars=#
 
-parspop = @parameters r₁=0.038 Kp₁=1.5 r₂=0.042 r₁₂=0 r₂₁=0 Kp₂=9.7 ml₁₂=10 ml₂₁=10 mp₁₂=10 mp₂₁=10  #population/migration dynamics
-parsecn = @parameters δ₁=0.05 δ₂=0.05 α₁=0.5 α₂=0.5 s₁=0.25 s₂=0.21 Cₘ₁=0.7 Cₘ₂=0.7 a₁=2.7 a₂=1.7 dmₓ=100 #economic dynamics
-parsear = @parameters re₁=0.1 re₂=0.1 eb=0.00004 u=0.0025 α=0.1 σ₁=0.03 σ₂=0.03 G₀=20 G₁=5 Gₘ=20 Tg=50 η=1 #earth system and climate 
-pars = [parspop;parsecn;parsear] #combine into a single vector.
+parspop = @parameters r₁ = 0.038 Kp₁ = 1.5 r₂ = 0.042 r₁₂ = 0 r₂₁ = 0 Kp₂ = 9.7 ml₁₂ = 10 ml₂₁ = 10 mp₁₂ = 10 mp₂₁ = 10  #population/migration dynamics
+parsecn = @parameters δ₁ = 0.05 δ₂ = 0.05 α₁ = 0.5 α₂ = 0.5 s₁ = 0.25 s₂ = 0.21 Cₘ₁ = 0.7 Cₘ₂ = 0.7 a₁ = 2.7 a₂ = 1.7 dmₓ = 100 #economic dynamics
+parsear = @parameters re₁ = 0.1 re₂ = 0.1 eb = 0.00004 u = 0.0025 α = 0.1 σ₁ = 0.03 σ₂ = 0.03 G₀ = 20 G₁ = 5 Gₘ = 20 Tg = 50 η = 1 #earth system and climate 
+pars = [parspop; parsecn; parsear] #combine into a single vector.
 
 #=define the state variables. Assign them to the name "vars" for convenience. Again, initial values are set in the 
 declaration. Alternatively, they can be set as follows:
@@ -28,24 +28,24 @@ u0=[P₁=>0.24,P₂=>0.24,K₁=>0,K₂=>0,G=>2.8,e₁=>0.0004,e₂=>0.0004,z=>0]
 
 Then the ODEProblem function can be called with u0.=#
 
-vars = @variables P₁(t)=0.24 P₂(t)=0.24 K₁(t)=0 K₂(t)=0 G(t)=2.8 e₁(t)=0.0004 e₂(t)=0.0004 z(t)=0
+vars = @variables P₁(t) = 0.24 P₂(t) = 0.24 K₁(t) = 0 K₂(t) = 0 G(t) = 2.8 e₁(t) = 0.0004 e₂(t) = 0.0004 z(t) = 0
 
 #=below is a differentiable threshold function that goes from 0
 to 1 as its argument goes from negative to positive. This keeps
 things well-behaved and physically meaninful (no negative stocks).
 This could be done in many different ways....=#
 
-θ₁(x,p,h) = (max(0,x))^p/(h^p + (max(0,x))^5)
-@register θ₁(x,p,h)
+θ₁(x, p, h) = (max(0, x))^p / (h^p + (max(0, x))^5)
+@register θ₁(x, p, h)
 
-θ(x) = θ₁(x,5,0.0001)
+θ(x) = θ₁(x, 5, 0.0001)
 @register θ(x)
 
 
 #=this is a differentiable form of max(x,0) - i.e. approximates it very 
 closely with no kink at zero.=#
 
-ϕ(x) = x*θ(x)
+ϕ(x) = x * θ(x)
 @register ϕ(x)
 
 #=economic subsystem. Society may use industrial technology
@@ -54,11 +54,11 @@ or a backstop technology that requires only labor inputs.=#
 #=industrial output regime - Cobb-Douglass Technology with constant
 returns to scale.=#
 
-Yᵢ(a,α,K,P) = a*((ϕ(K))^α)*((ϕ(P))^(1-α))
-@register Yᵢ(a,α,K,P)
+Yᵢ(a, α, K, P) = a * ((ϕ(K))^α) * ((ϕ(P))^(1 - α))
+@register Yᵢ(a, α, K, P)
 
-Yᵢ₁ = Yᵢ(a₁,α₁,K₁,P₁)
-Yᵢ₂ = Yᵢ(a₂,α₂,K₂,P₂)
+Yᵢ₁ = Yᵢ(a₁, α₁, K₁, P₁)
+Yᵢ₂ = Yᵢ(a₂, α₂, K₂, P₂)
 
 
 #= background production regime (labor based production)
@@ -66,18 +66,18 @@ is given by ab₁P₁ but we assume the multiplier is 1. We then
 use the threshold function to switch to industrical production
 only when it is more productive than the backdrop technology.=#
 
-Y(P,Y) = P + ϕ(Y-P)
-@register Y(P,K)
+Y(P, Y) = P + ϕ(Y - P)
+@register Y(P, K)
 
-Y₁ = Y(P₁,Yᵢ₁)
-Y₂ = Y(P₂,Yᵢ₂)
+Y₁ = Y(P₁, Yᵢ₁)
+Y₂ = Y(P₂, Yᵢ₂)
 
 #=emissions from backdrop or industrialized. Assume eb=0.1*e1 in the 
 scenario below, but this is arbitrary. The 'r' in er refers to 
 'realized' caron intensity depending on which technology is used=#
 
-er₁ = θ(P₁-Yᵢ₁)*eb + θ(Yᵢ₁-P₁)*e₁
-er₂ = θ(P₂-Yᵢ₂)*eb + θ(Yᵢ₂-P₂)*e₂
+er₁ = θ(P₁ - Yᵢ₁) * eb + θ(Yᵢ₁ - P₁) * e₁
+er₂ = θ(P₂ - Yᵢ₂) * eb + θ(Yᵢ₂ - P₂) * e₂
 
 #decarbonization. e1 and e2 obey simple odes with exponential decay. z is a Markov
 # variable that turns on decarobonization (represents a policy change
@@ -95,8 +95,8 @@ er₂ = θ(P₂-Yᵢ₂)*eb + θ(Yᵢ₂-P₂)*e₂
 #=investment depends on disposable income... which is income
 beyond a subsistence level Cₘ=#
 
-Yd₁ = ϕ(Y₁ - P₁*Cₘ₁)
-Yd₂ = ϕ(Y₂ - P₂*Cₘ₂)
+Yd₁ = ϕ(Y₁ - P₁ * Cₘ₁)
+Yd₂ = ϕ(Y₂ - P₂ * Cₘ₂)
 
 
 
@@ -117,8 +117,8 @@ For the continous portion we assume  damage = σ₁*exp(G-G₁)*K₁, i.e.
 damages grow expoentially beyond threshold G₁.  The next stage of
 development is to model the stochastic portion.=#
 
-dm₁ = σ₁*dmₓ*exp(G-G₁)/(dmₓ + exp(G-G₁))
-dm₂ = σ₂*dmₓ*exp(G-G₁)/(dmₓ + exp(G-G₁))
+dm₁ = σ₁ * dmₓ * exp(G - G₁) / (dmₓ + exp(G - G₁))
+dm₂ = σ₂ * dmₓ * exp(G - G₁) / (dmₓ + exp(G - G₁))
 
 
 #= Popuation dynamics.  Local population is logistic growth, migration 
@@ -130,7 +130,7 @@ to migrate (per capita income) and relative welfare perceptions between regions
 
 #define standard logistic
 
-λ(x) = exp(x)/(exp(x)+1)
+λ(x) = exp(x) / (exp(x) + 1)
 @register λ(x)
 
 # Then for the migration pressure we get (various attempts and possibilities):
@@ -144,13 +144,13 @@ to migrate (per capita income) and relative welfare perceptions between regions
 #define a function to calculate the percentage difference between to numbers
 #for code readability
 
-pd(a,b) = (a-b)/(a+b)
-@register pd(a,b)
+pd(a, b) = (a - b) / (a + b)
+@register pd(a, b)
 
 #not for the migration pressures....
 
-m₁₂ = mp₁₂*pd(Y₂/P₂,Y₁/P₁) - ml₁₂
-m₂₁ = mp₂₁*pd(Y₁/P₁,Y₂/P₂) - ml₂₁
+m₁₂ = mp₁₂ * pd(Y₂ / P₂, Y₁ / P₁) - ml₁₂
+m₂₁ = mp₂₁ * pd(Y₁ / P₁, Y₂ / P₂) - ml₂₁
 
 
 # and for the probability of migration, pᵢⱼ, we get
@@ -164,17 +164,17 @@ p₂₁ = λ(m₂₁)
 #differential equations
 
 eqs = [
-    D(P₁) ~ r₁*P₁*(1 - P₁/Kp₁) - r₁₂*P₁*p₁₂ + r₂₁*P₂*p₂₁, #human population dynamics 
-    D(P₂) ~ r₂*P₂*(1 - P₂/Kp₂) + r₁₂*P₁*p₁₂ - r₂₁*P₂*p₂₁, # standard logistic.
-    D(K₁) ~ s₁*Yd₁ - δ₁*K₁ - dm₁*K₁, #change in capital stock = 
-    D(K₂) ~ s₂*Yd₂ - δ₂*K₂ - dm₂*K₂, # savings-depreciation-damages
-    D(G)  ~ er₁*Y₁ + er₂*Y₂ - u*(G-2.8) + α*θ(G-G₀)*θ₁(Gₘ-G,3,1), # global externality.
-    D(z)  ~ η*θ(G-Tg)*(1-z), #initiate decarbonization
-    D(e₁) ~ -z*re₁*e₁, # decarbonization in region 1
-    D(e₂) ~ -z*re₂*e₂, # decarbonization in region 2
-    ]
+    D(P₁) ~ r₁ * P₁ * (1 - P₁ / Kp₁) - r₁₂ * P₁ * p₁₂ + r₂₁ * P₂ * p₂₁, #human population dynamics 
+    D(P₂) ~ r₂ * P₂ * (1 - P₂ / Kp₂) + r₁₂ * P₁ * p₁₂ - r₂₁ * P₂ * p₂₁, # standard logistic.
+    D(K₁) ~ s₁ * Yd₁ - δ₁ * K₁ - dm₁ * K₁, #change in capital stock = 
+    D(K₂) ~ s₂ * Yd₂ - δ₂ * K₂ - dm₂ * K₂, # savings-depreciation-damages
+    D(G) ~ er₁ * Y₁ + er₂ * Y₂ - u * (G - 2.8) + α * θ(G - G₀) * θ₁(Gₘ - G, 3, 1), # global externality.
+    D(z) ~ η * θ(G - Tg) * (1 - z), #initiate decarbonization
+    D(e₁) ~ -z * re₁ * e₁, # decarbonization in region 1
+    D(e₂) ~ -z * re₂ * e₂, # decarbonization in region 2
+]
 
-@named sys = ODESystem(eqs,t,vars,pars);
+@named sys = ODESystem(eqs, t, vars, pars);
 
 #= the variable sys is a symbolic object containing a mathematical representation
 of the problem. Next, translate the symbolic problem into code to be solved numerically.
@@ -182,19 +182,19 @@ using ODEProblem(systemname,initial conditions, integration time span, parameter
 Note that since we havae set the default values for vars and pars in sys, we send in
 blank placeholders in the form of [].  So we create the problem, then sovle it.=#
 
-prob = ODEProblem(sys, [], (0.0,600), [])
-sol = solve(prob,saveat=1.0)
+prob = ODEProblem(sys, [], (0.0, 600), [])
+sol = solve(prob, saveat=1.0)
 
 #=Now plot the results using Plots with the GR backend.  This is the simplest but does not
 produce as high quality or flexible output as do other backends.=#
 
 plot(sol, idxs=[Y₁]);
 plot!(sol, idxs=[Y₂]);
-p1=plot!(sol, idxs=[10*G],xlabel="Time", ylabel="GNI");
-plot(sol,idxs=[P₁]);
-p2=plot!(sol,idxs=[P₂],xlabel="Time", ylabel="Population"); 
-p3=plot(sol[Y₁/P₁],sol[Y₂/P₂], xlabel="per capita GNI, region 1",
-ylabel="per capita GNI, region 2");
-p4=plot(sol[G*100],sol[Y₁+Y₂], xlabel="per capita GNI, region 1",
-ylabel="per capita GNI, region 2");
-plot(p2, p1, p3, p4, layout=(2,2), legend=false, size=(750,500))
+p1 = plot!(sol, idxs=[10 * G], xlabel="Time", ylabel="GNI");
+plot(sol, idxs=[P₁]);
+p2 = plot!(sol, idxs=[P₂], xlabel="Time", ylabel="Population");
+p3 = plot(sol[Y₁/P₁], sol[Y₂/P₂], xlabel="per capita GNI, region 1",
+    ylabel="per capita GNI, region 2");
+p4 = plot(sol[G*100], sol[Y₁+Y₂], xlabel="per capita GNI, region 1",
+    ylabel="per capita GNI, region 2");
+plot(p2, p1, p3, p4, layout=(2, 2), legend=false, size=(750, 500))
