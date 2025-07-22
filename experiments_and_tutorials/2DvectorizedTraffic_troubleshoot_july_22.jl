27aec991cdd9cdc3c3a19371830c76182e75949f
Aurora Cossairt <a.cossairt@outlook.com>
2025-07-22 17:36:09 -0600

    New troubleshooting notebook, July 22

---
+++ experiments_and_tutorials/2DvectorizedTraffic_troubleshoot_july_22.jl
@@ -0,0 +1,67 @@
+# Fast call for helper functions to run model
+aurora_path_to_help_funcs = "/Users/auroracossairt/opt/traffic_air_quality_modeling/julia_model/help_funcs.jl"
+marty_path_to_help_funcs = "/Users/janderie/work/cmpting/git/traffic_air_quality_modeling/julia_model/help_funcs.jl"
+include(marty_path_to_help_funcs) # CHANGE FOR YOURSELF
+using .HelpFuncs
+using ModelingToolkit, DifferentialEquations, Plots, LinearAlgebra
+
+#=
+Set initial conditions, parameters, and other arguments to pass to model solver
+=#
+
+Np = 2 # Number of patches
+Nc = 1 # Number of corridors
+
+# Set initial conditions
+kp_init = [1.0; 0.0]
+kc_init = zeros(Np, Np, Nc)
+u_init = 1
+v_init = 0
+
+# Set timescale
+my_period = 1440    # 24 hours, in minutes
+my_t_end = my_period     # Change if you only want to examine part of a day (< my_period), or multiple days (> my_period)
+scale = 1440 / my_period # if using a period other than 1440, this will rescale the other parameters
+
+# Set parameters
+my_α = [1]   # Tolerance for congestion (for now, assume same for patch 1 and patch 2)
+kc_jam_base = 10
+my_kc_jam = kc_jam_base * ones(Np, Np, Nc)  # Inverse road capacity (for now, assume same for all corridors)
+my_kc_jam[[CartesianIndex(i, i, k) for i in 1:Np, k in 1:Nc]] .= 1e9 # set diagonal values to almost Inf
+display(my_kc_jam)
+
+# TEST: set C_init to C_jam
+#kc_init[1,2,1] = 1 ./ my_kc_jam[1,2,1] + 0.05 # Should be 1/20
+display(kc_init)
+
+# Arguments for demand function (which is f() = L / (1 + exp(-r * (x-x0))) )
+demand_function = "periodic_logistic" # Default option is "static"
+my_L = 0.6
+my_r = 100
+my_x0 = 0.99
+my_shift = 0 * 60    # in units of minutes, set to 0 for no shift
+
+# Parameters for calculating space-mean speed
+my_v_f = 90
+my_a = 20
+
+#=
+Build model, solve problem, plot results, and save
+=#
+
+model, prob, sol, plt = HelpFuncs.build_symbolic_model(Np=Np, Nc=Nc,
+    my_kp=kp_init, my_kc=kc_init, my_u=u_init, my_v=v_init, my_α=my_α, my_kc_jam=my_kc_jam,
+    γ_version=demand_function, my_period=my_period, t_end=my_t_end, my_v_f=my_v_f, my_a=my_a, my_L=my_L,
+    my_r=my_r, my_x0=my_x0, my_shift=my_shift)
+
+# Suffix to specify test
+suffix = "_new_speed_density_flux" #"_original" #"_shut_off_entry_flux" #"_new_speed_density" #"_shut_off_entry_flux" #"_shut_off_exit_flux"
+
+# Save plot
+if demand_function == "periodic_logistic"
+    savefig(plt, "traffic_pop_curve_diurnal_test_" * demand_function * "_Nc_$Nc" * "_kc_jam_base_$kc_jam_base" * "_t_end_$my_t_end" * "_L_$my_L" * "_r_$my_r" * "_x0_$my_x0" * "_shift_$my_shift" * "_timescale_$scale" * suffix * ".png")
+else
+    savefig(plt, "traffic_pop_curve_diurnal_test_" * demand_function * "_Nc_$Nc" * "_kc_jam_base_$kc_jam_base" * "_t_end_$my_t_end" * "_timescale_$scale" * suffix * ".png")
+end
+
+display(plt)
--- julia_model/help_funcs.jl
+++ julia_model/help_funcs.jl
@@ -16,14 +16,341 @@ using IfElse
 #####################################
 
 function speed_density_curve(; v_f, a, C, kc_jam)
-    C_half = (1 / kc_jam) / 2
-    return (v_f / ((pi / 2 .- atan.(a .* (0 .- C_half))) / pi)) .* (pi / 2 .- atan.(a .* (C .- C_half))) / pi
+    C_half = (1 ./ kc_jam) ./ 2
+    return (v_f ./ ((pi / 2 .- atan.(a .* (0 .- C_half))) ./ pi)) .* (pi / 2 .- atan.(a .* (C .- C_half))) ./ pi
+end
+
+function fake_speed_density_curve(C, kc_jam)
+    return exp.(-kc_jam .* C)
 end
 
 ##################
 # THE MAIN EVENT #
 ##################
 
+# New version, ensure that flux functions are based on speed-density function
+function build_symbolic_model(; Np=2, Nc=1, my_kp, my_kc, my_u=1, my_v=0, my_α,
+    my_kc_jam, my_period=1440, γ_version="static", t_end=120, my_v_f=90, my_a=1, my_L=0.6, my_r=10,
+    my_x0=0.8, my_shift=0, x_pos=0.05, y_pos=0.15)
+    #=
+    Arguments
+        - Np (int): number of patches
+        - Nc (int): maximum number of corridors between any two patches
+        - my_kp (array of floats): initial conditions for variable kp (density in patches)
+        - my_kc (array of floats): initial conditions for variable kc (density in corridors)
+        - my_u (int): initial conditions for variable u (the cos var of the dynamical clock)
+        - my_v (int): initial conditions for variable v (the sin var of the dynamical clock)
+        - my_α (array of floats): values for parameter α
+        - my_kc_jam (array of floats): values for parameter kc_jam
+            - Note: make sure the values along the diagonal of this matrix are 1e9,
+                    or else you will have vehicles leaking into non-existent "self-loops."
+                    Can't use `Inf` because initial conditions may include 0, leading to
+                    Inf * 0 = NaN, and NaN objects will break the solver.
+        - my_period (int): value for parameter period (for the dynamical clock)
+        - γ_version (string): user chooses which version of demand function to use
+            - "static": (default) d1 = 1 always, d2 = 0 always
+            - "periodic_logistic": uses v as the input x to the logistic equation
+                    f(x) = L / (1 + exp(-k*(x-x0))).
+                    This creates a well behaved periodic function which can be 
+                    parameterized to look like a square wave. Using function v_shifted, 
+                    the input can also be right or left shifted (by an amount 
+                    `my_shift`) to specify peak demand hour
+        - t_end (int): end time for dynamical solver
+            - Note: this is not equivalent to number of time steps. That can be
+                specified by passing an argument `dt` in the function `solve()`, 
+                otherwise it is determined adaptively by the solver (of type Tsit5)
+        - my_v_f (float): free-flow-velocity 
+            (argument to be passed to calc_space_mean_speed_alternative)
+        - my_a (float): tuning parameter for speed-density 
+            (argument to be passed to calc_space_mean_speed_alternative)
+        - my_L: value for parameter L in the logistic demand function
+        - my_r: value for parameter k in the logistic demand function
+        - my_x0: value for parameter L in the logistic demand function
+        - my_shift: value for parameter `shift` in function v_shifted(). Specifies
+            how much to shift v (aka sin(t)) left or right shift before passing to the
+            demand function f()
+        - x_pos: accessory for annotating demand function plot:
+            relative positioning along x axis
+        - y_pos: accessory for annotating demand function plot:
+            relative positioning along y axis
+    =#
+
+    # Create variables for population model and internal "clock" model
+    @variables kp(t)[1:Np]             # population density in patches
+    @variables kc(t)[1:Np, 1:Np, 1:Nc] # population density in corridors
+    @variables γ(t)[1:Np]              # fraction of pop wanting to leave each patch
+    @variables u(t)                    # cos(t) (for dynamical clock)
+    @variables v(t)                    # sin(t) (for dynamical clock)
+
+    # Create parameters
+    @parameters α[1:Np]                # tolerance for congestion (per patch)
+    @parameters kc_jam[1:Np, 1:Np, 1:Nc]   # inverse road capacity (per corridor)
+    @parameters period                 # period for dynamical clock
+
+    #=
+    Define demand function (to create "γ" for Flux functions):
+        This is the logistic equation, which is smooth, continuous, and well-behaved
+        If you pass a periodic function (like v) as the argument `x`, then f(x) will look
+        like a (well-behaved) square wave, peaking at the same place as where the
+        periodic function peaks.
+    =#
+    f(x, r, x_0, L) = L / (1 + exp(-r * (x - x_0)))
+
+    #=
+    Shift demand function left or right: 
+        Using trig identity: sin(a-b) = sin(a)cos(b) - cos(a)sin(b)
+        i.e. if `my_period=1440` (24 hours, in minutes), then my `v` will peak at `t=360` 
+        (aka 6am). If I want morning demand to also peak at 6am, then leave `shift=0`. 
+        However, if I instead want morning demand to peak at 9am, then I should pass 
+        `shift=180` (3 hours * 60 minutes) to `v_shifted()`. The result will then
+        automatically be passed to `f()`` to generate `γ`.
+    =#
+    v_shifted(shift) = v * cos(2 * π * shift / period) - u * sin(2 * π * shift / period)
+
+    if γ_version == "periodic_logistic"
+        γ_eqs = [
+            γ[1] ~ f(v_shifted(my_shift), my_r, my_x0, my_L),
+            γ[2] ~ f(-v_shifted(my_shift), my_r, my_x0, my_L)
+        ]
+    elseif γ_version == "static"
+        γ_eqs = [
+            γ[1] ~ 1,
+            γ[2] ~ 0,
+        ]
+    end
+
+    #=
+    Rescale time in terms of a day that is 1440 minutes
+    =#
+    time_rescale = 1440 / my_period
+
+    #=
+    Define the corridor flux matrices:
+        Notice the use of `.` notation before each arithmetic operation, but NOT before 
+        `=`. This is because `.=` only makes sense if the LHS of the equation is an 
+        existing vector (or matrix) which has exactly same shape as the RHS.
+
+        What follows is a bunch of attempts at creating the flux functions. 
+        All versions lead to errors except for Versions 1 and 2.
+
+        Note that kc_{jam,half} = (1 ./ kc_jam) ./ 2. This is the inflection point of our
+        function, where the average space-mean speeds have dropped to 1/2 of the
+        free-flow velocity v_f.
+    =#
+
+    # Version 0.0: original
+    EnFlx(kp, kc, γ, α, kc_jam) = exp.(-α .* kc_jam .* kc) .* kp .* γ .* time_rescale
+    ExFlx(kc, kc_jam) = exp.(-kc_jam .* kc) .* kc .* time_rescale
+
+    # Version 0.1: testing whether an external function would have worked for 
+    #   original equation (spoiler: it wouldn't have worked)
+    #EnFlx(kp, kc, γ, α, kc_jam) = fake_speed_density_curve.(kc, kc_jam) .* kp .* γ .* time_rescale
+    #ExFlx(kc, kc_jam) = fake_speed_density_curve.(kc, kc_jam) .* kc .* time_rescale
+
+    # Version 1.0: no external function
+    #EnFlx(kp, kc, γ, α, kc_jam) = ((my_v ./ ((pi / 2 .- atan.(my_a .* (0 .- (1 ./ kc_jam) ./ 2))) ./ pi)) .* (pi ./ 2 .- atan.(my_a .* (kc .- (1 ./ kc_jam) ./ 2))) ./ pi) .* kp .* γ .* time_rescale
+    #ExFlx(kc, kc_jam) = ((my_v ./ ((pi / 2 .- atan.(my_a .* (0 .- (1 ./ kc_jam) ./ 2))) ./ pi)) .* (pi ./ 2 .- atan.(my_a .* (kc .- (1 ./ kc_jam) ./ 2))) ./ pi) .* kc .* time_rescale
+
+    # Version 1.1: replace `my_v_f` with `1.0` so that vehicles don't flow so fast
+    #EnFlx(kp, kc, γ, α, kc_jam) = ((1 ./ ((pi / 2 .- atan.(my_a .* (0 .- (1 ./ kc_jam) ./ 2))) ./ pi)) .* (pi ./ 2 .- atan.(my_a .* (kc .- (1 ./ kc_jam) ./ 2))) ./ pi) .* kp .* γ .* time_rescale
+    #ExFlx(kc, kc_jam) = ((1 ./ ((pi / 2 .- atan.(my_a .* (0 .- (1 ./ kc_jam) ./ 2))) ./ pi)) .* (pi ./ 2 .- atan.(my_a .* (kc .- (1 ./ kc_jam) ./ 2))) ./ pi) .* kc .* time_rescale
+
+    # Version 1.2.: multiply everything by kc_half
+    #   This ensures that the "loops" (e.g. corridor from p1 to p1), will take no vehicles
+    #   (this works because kc_jam = 1e9 for these corridors, so they have kc_half=0)
+    #EnFlx(kp, kc, γ, α, kc_jam) = ((1 ./ kc_jam) ./ 2) .* ((1.0 ./ ((pi / 2 .- atan.(my_a .* (0 .- (1 ./ kc_jam) ./ 2))) ./ pi)) .* (pi ./ 2 .- atan.(my_a .* (kc .- (1 ./ kc_jam) ./ 2))) ./ pi) .* kp .* γ .* time_rescale
+    #ExFlx(kc, kc_jam) = ((1 ./ kc_jam) ./ 2) .* ((1.0 ./ ((pi / 2 .- atan.(my_a .* (0 .- (1 ./ kc_jam) ./ 2))) ./ pi)) .* (pi ./ 2 .- atan.(my_a .* (kc .- (1 ./ kc_jam) ./ 2))) ./ pi) .* kc .* time_rescale
+
+    # Version 2.0: calculate v in external function, then multiply
+    #v = speed_density_curve.(my_v_f, my_a, kc, kc_jam)
+    #EnFlx(kp, kc, γ, α, kc_jam) = ((1 ./ kc_jam) ./ 2) .* v .* kp .* γ .* time_rescale
+    #ExFlx(kc, kc_jam) = ((1 ./ kc_jam) ./ 2) .* v .* kc .* time_rescale
+
+    #=
+    Define equations for model:
+        Notice there are only two equations in the population model, regardless of 
+        Np and Nc. The dynamical clock model has 3 or more equations (for u, v, and γ)
+        Question: does it matter if u, v, and γ are evaluated before or after kp and kc?
+        Notice: we only used "collect()" in the equation for D.(c).
+        For D.(p), we summed over the columns of the EnFlx matrix and over the columns 
+        of the ExFlx matrix, then took their difference. If you don't do this (summing),
+        you will get an error because you will be attempting to save an object of shape 
+        (2,2,1) into an object of shape (2,). (Assuming Np=2)
+    =#
+    eqs = [
+        D.(kp) ~ [sum(ExFlx(kc, kc_jam)[:, i, :]) for i in 1:Np] .- [sum(EnFlx(kp, kc, γ, α[1], kc_jam)[i, :, :]) for i in 1:Np],
+        D.(kc) ~ collect(-ExFlx(kc, kc_jam) + EnFlx(kp, kc, γ, α[1], kc_jam)),
+        D(u) ~ u * (1 - u^2 - v^2) - (2 * pi / period) * (v),
+        D(v) ~ v * (1 - u^2 - v^2) + (2 * pi / period) * (u),
+        γ_eqs...   # <-- include the triple dots to splice the equations into the list
+    ]
+
+    # Build model symbolically
+    @mtkbuild model = ODESystem(eqs, t)
+
+    #=
+    Solve problem, passing initial conditions for variables, a time range, and parameter
+    values.
+        Notice we don't pass `γ` as a parameter or variable, because it is defined 
+        within our equations as an "observed" variable, so it does not require 
+        initial conditions.
+    =#
+    prob = ODEProblem(model, [kp => my_kp, kc => my_kc, u => my_u, v => my_v], (0.0, t_end), [α => my_α, kc_jam => my_kc_jam, period => my_period])
+
+    # Solve problem
+    sol = solve(prob, Tsit5())
+
+    #=
+    Plot results:
+        Subplot 1: population (densities) of patches and corridors vs. time (in hours)
+        Subplot 2: average vehicle speeds vs. time (in hours)
+        Subplot 3: diurnal clock (`u` and `v`) and demand function `γ` vs. time (in hours)
+        Subplot 4: speed-density curve
+    =#
+
+    # Define accessories for plots
+    my_colors = [:darkcyan, :chocolate2, :purple, :dodgerblue4, :orangered2]
+    my_linestyles = [:dash, :dashdotdot, :dot]
+    legend_loc = Nc > 1 || Np > 2 ? :right : :topleft
+
+    # Set up subplots
+    plt1 = plot(legendfontsize=10, xtickfontsize=12, ytickfontsize=12, titlefontsize=18, xguidefontsize=14, yguidefontsize=14, ylabel="population", legend=legend_loc, legend_background_color=RGBA(1, 1, 1, 0.5)) # palette=palette(my_colors)
+    plt2 = plot(legendfontsize=10, xtickfontsize=12, ytickfontsize=12, xguidefontsize=14, yguidefontsize=14, xlabel="t", ylabel="v (km/h)", legend=:right, legend_background_color=RGBA(1, 1, 1, 0.5)) #, ylims=(35, 50)) # palette=palette(my_colors)
+    plt3 = plot(legendfontsize=10, xtickfontsize=12, ytickfontsize=12, xguidefontsize=14, yguidefontsize=14, xlabel="t", legend=:right, legend_background_color=RGBA(1, 1, 1, 0.5), palette=:lightrainbow)
+
+    # Create subplot 3, diurnal demand pattern
+    plt3 = plot!(plt3, sol.t ./ 60, sol[u], label="u", linewidth=2)
+    plt3 = plot!(plt3, sol.t ./ 60, sol[v], label="v", linewidth=2)
+    plt3 = title!(plt3, "Demand function: " * γ_version)
+
+    # Plot patch-specific values (subplots 1 and 3)
+    for i in 1:Np
+        plt1 = plot!(plt1, sol.t ./ 60, sol[kp[i]], label="kp$i", linewidth=3, color=my_colors[i])
+        plt3 = plot!(plt3, sol.t ./ 60, sol[γ[i]], label="γ$i", linewidth=3, linestyle=:dash, color=my_colors[i])
+    end
+
+    # Track total populations to ensure population conserved
+    total_pop = zeros(length(sol.t))
+    total_pop_patches = zeros(length(sol.t))
+
+    # Plot corridor-specific values (subplots 1 and 2)
+    for j in 1:Np # to patch j
+        my_colors1 = palette([my_colors[j], :snow2], Np * Nc + 2) # padding because I won't use the first or last colors in this palette
+
+        # Update total population counts
+        total_pop_patches .+= sol[kp[j]]
+        total_pop .+= sol[kp[j]]
+
+        for i in 1:Np # from patch i
+            if j != i # skip diagonals (non-existent "loop" corridors)
+                for k in 1:Nc # via corridor k
+                    this_kc_jam = my_kc_jam[i, j, k]
+                    this_color = my_colors1[i+k] # always skips the first (brightest) shade
+
+                    # Plot corridor population densities (subplot 1)
+                    plt1 = plot!(plt1, sol.t ./ 60, sol[kc[i, j, k]],
+                        label="c$k, p$i→p$j (C_jam=1/$this_kc_jam)", linewidth=3,
+                        linestyle=my_linestyles[k], color=this_color)
+
+                    # Plot jam densities for each corridor (subplot 1)
+                    plt1 = hline!(plt1, [1 ./ this_kc_jam],
+                        label="C_jam=1/$this_kc_jam", linewidth=1,
+                        linestyle=my_linestyles[k], color=this_color)
+
+                    # Plot average vehicle speeds in each corridor (subplot 2)
+                    # Would be nice if I could do this outside the for loop...
+                    my_C_speeds = speed_density_curve.(v_f=my_v_f, a=my_a, C=sol[kc[i, j, k]], kc_jam=my_kc_jam[i, j, k])
+
+                    plt2 = plot!(plt2, sol.t ./ 60, my_C_speeds,
+                        label="u: c$k, p$i → p$j",
+                        title="average speeds (assume my_v_f=90 kmh)",
+                        linewidth=3, linestyle=my_linestyles[k], color=this_color)
+
+                    # Update total population count
+                    total_pop .+= sol[kc[i, j, k]] # If this dips below 1, you are probably losing population to the loops
+                end
+            end
+        end
+    end
+
+    # Plot total populations - should always be 1! (subplot 1)
+    plt1 = plot!(plt1, sol.t ./ 60, total_pop, label="total population", color="black")
+
+    # Set title
+    if Nc > 1
+        title!(plt1, "Daily commute: $Np patches, $Nc corridors")
+    else
+        title!(plt1, "Daily commute: $Np patches, $Nc corridor")
+    end
+    plt1 = title!(plt1, "Daily commute: $Np patches, $Nc $(Nc > 1 ? "corridors" : "corridor")")
+
+    # Annotate subplot 3 (specify demand function)
+    if γ_version == "periodic_logistic"
+        my_shift_normalized = my_shift / 60
+        # Create parameter text
+        param_text = """
+        Demand function:
+        f = L / (1 + exp^(-r*(x-x0)))
+        Parameters:
+        L: $my_L | r: $my_r | x0: $my_x0
+        x shifted by: $my_shift_normalized hours
+        """
+
+        # Use relative positioning (0-1 scale)
+        xlims = Plots.xlims(plt3)
+        ylims = Plots.ylims(plt3)
+        x_actual = xlims[1] + (xlims[2] - xlims[1]) * x_pos
+        y_actual = ylims[1] + (ylims[2] - ylims[1]) * y_pos
+
+        plt3 = annotate!(plt3, (x_actual, y_actual, text(param_text, :left, 8, :black, RGBA(0, 0, 0, 1))))
+    end
+
+
+    # Create subplot 4, speed-density curve (example for c1: p1 → p2)
+    my_C_range = [0.0:0.01:1.0;]
+    my_speeds_test = speed_density_curve.(v_f=my_v_f, a=my_a, C=my_C_range, kc_jam=my_kc_jam[1, 2, 1])
+    my_speeds_direct = speed_density_curve.(v_f=my_v_f, a=my_a, C=sol[kc[1, 2, 1]], kc_jam=my_kc_jam[1, 2, 1])
+    my_new_speeds_test = fake_speed_density_curve.(my_C_range, my_kc_jam[1, 2, 1])
+    my_new_speeds_direct = fake_speed_density_curve.(sol[kc[1, 2, 1]], my_kc_jam[1, 2, 1])
+    plt4 = plot(my_C_range, my_speeds_test, label="speed-density relation", title="speed-density curve: c1, p1 → p2", ylabel="avg speed", xlabel="density")
+    plt4 = plot!(plt4, my_C_range, my_new_speeds_test, label="old speed-density relation")
+    plt4 = vline!(plt4, [(1 ./ my_kc_jam[1, 2, 1]) ./ 2], label="my C_half")
+    plt4 = plot!(plt4, sol[kc[1, 2, 1]], my_speeds_direct, label="my speeds (directly calculated)", linewidth=3)
+    plt4 = plot!(plt4, sol[kc[1, 2, 1]], my_new_speeds_direct, label="my fake speeds (directly calculated)", linewidth=3)
+
+    # --- Combine into final layout ---
+    layout = @layout [
+        a d
+        b _
+        c _
+    ]
+
+    plt = plot(plt1, plt4, plt2, plt3, layout=layout, size=(1200, 1200))
+
+    #plt = plot(plt1, plt2, plt3, layout=@layout([a; b; c]), link=:x, size=(800, 1200))
+    return model, prob, sol, plt
+
+end
+
+
+######################
+# Get emission rates #
+######################
+
+function get_emission_rates(x_1) # how to specify that this function should work with vector arguments?
+    x_2 = ifelse.(i * f * x_1 .> 60, x_1 - 60 * i * f * x_1, 0.0) # what are i and f?
+    x_3 = ifelse.(i * f * x_1 .> 80, x_1 - 80 * i * f * x_1, 0.0)
+    Y = 0.027 + 6.8e-05 * x_1 + 0.00032 * x_2 + 0.00050 * x_3
+    return Y
+end
+
+#################################
+# OLD STUFF, DON'T WANT TO LOSE #
+#################################
+
+#=
+Original model with flux functions and speed functions independent
+=#
 function build_symbolic_model_diurnal(; Np=2, Nc=1, my_kp, my_kc, my_u=1, my_v=0, my_α,
     my_kc_jam, my_period=1440, γ_version="static", t_end=120, my_v_f=90, my_a=1, my_L=0.6, my_r=10,
     my_x0=0.8, my_shift=0, x_pos=0.05, y_pos=0.15)
@@ -290,24 +617,6 @@ function build_symbolic_model_diurnal(; Np=2, Nc=1, my_kp, my_kc, my_u=1, my_v=0
 
 end
 
-
-######################
-# Get emission rates #
-######################
-
-function get_emission_rates(x_1) # how to specify that this function should work with vector arguments?
-    x_2 = ifelse.(i * f * x_1 .> 60, x_1 - 60 * i * f * x_1, 0.0) # what are i and f?
-    x_3 = ifelse.(i * f * x_1 .> 80, x_1 - 80 * i * f * x_1, 0.0)
-    Y = 0.027 + 6.8e-05 * x_1 + 0.00032 * x_2 + 0.00050 * x_3
-    return Y
-end
-
-end  # module
-
-#################################
-# OLD STUFF, DON'T WANT TO LOSE #
-#################################
-
 #=
 Not sure why this function didn't work? It led to speeds very very close to v_f,
 even at high densities (above jam density)
@@ -349,6 +658,8 @@ function calc_space_mean_speed_alternative(v_f, kc, my_kc_jam; a=0.5, Np=2, Nc=2
 
     # \frac{-v}{\pi}\arctan\left(av\cdot\left(\tan\left(\pi\left(x+.5\right)\right)\ -\ h\right)\right)
 end
+
+end  # module
 
 #=
 function build_symbolic_model_diurnal_NEW_NOTATION(; make_prob=true, make_plot=true, Np=2, Nc=1,
