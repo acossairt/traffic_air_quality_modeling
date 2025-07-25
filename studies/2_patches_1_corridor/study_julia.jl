21a713c5c534cccb34d3eee403a0196131603ad2
John <m.anderies@asu.edu>
2025-07-25 16:07:17 -0700

    minor edits

--- studies/2_patches_1_corridor/study_julia.jl
+++ studies/2_patches_1_corridor/study_julia.jl
@@ -1,15 +1,16 @@
 # Fast call for helper functions to run model
-aurora_path_to_model = "/Users/auroracossairt/opt/traffic_air_quality_modeling/julia_model/traffic_model.jl"
-aurora_path_to_plotting_funcs = "/Users/auroracossairt/opt/traffic_air_quality_modeling/julia_model/traffic_plots.jl"
-marty_path_to_model = "/Users/janderie/work/cmpting/git/traffic_air_quality_modeling/julia_model/julia_model.jl"
-include(aurora_path_to_model) # CHANGE FOR YOURSELF
-include(aurora_path_to_plotting_funcs)
+#aurora_path_to_model = "/Users/auroracossairt/opt/traffic_air_quality_modeling/julia_model/traffic_model.jl"
+#aurora_path_to_plotting_funcs = "/Users/auroracossairt/opt/traffic_air_quality_modeling/julia_model/traffic_plots.jl"
+marty_path_to_model = "/Users/janderie/work/cmpting/git/traffic_air_quality_modeling/julia_model/traffic_model.jl"
+marty_path_to_plotting_funcs = "/Users/janderie/work/cmpting/git/traffic_air_quality_modeling/julia_model/traffic_plots.jl"
+include(marty_path_to_model) # CHANGE FOR YOURSELF
+include(marty_path_to_plotting_funcs) # CHANGE FOR YOURSELF
 
 # For some reason doesn't work if I include and also call the module? 
 # So I just commented out the include
 
-using ModelingToolkit, DifferentialEquations, Plots, LinearAlgebra, Printf
-
+using Printf, ModelingToolkit, DifferentialEquations, LinearAlgebra, Interpolations
+using IfElse, Plots
 using .traffic_model
 using .traffic_plots
 
@@ -37,7 +38,7 @@ Nc = 1
 my_period = 1440         # 24 hours, in minutes
 t_end = my_period        # Change if you only want to examine part of a day (< my_period), or multiple days (> my_period)
 scale = 1440 / my_period # if using a period other than 1440, this will rescale the other parameters
-tspan = (0.0, t_end);    #time span in years.
+tspan = (0.0, t_end);    #time span in minutes.
 
 # Set initial conditions
 kp_init = [1.0; 0.0]
@@ -56,8 +57,13 @@ u0 = [kc => kc_init, kp => kp_init, γ => γ_init, u => u_init, v => v_init];
 println("Setting up parameters...")
 # Set parameters
 my_α = [1]   # Tolerance for congestion (for now, assume same for patch 1 and patch 2)
+<<<<<<< HEAD
+kc_half_jam_base = 150
+my_kc_half_jam = (1 / kc_half_jam_base) / 2 * ones(Np, Np, Nc)  # 1/2 jam density for each corridor
+=======
 kc_half_jam_base = 10
 my_kc_half_jam = ((1 / kc_half_jam_base) / 2) * ones(Np, Np, Nc)  # 1/2 jam density for each corridor
+>>>>>>> 6733eb0a8aff0380423a1d8620d73528cb191cf1
 my_kc_half_jam[[CartesianIndex(i, i, k) for i in 1:Np, k in 1:Nc]] .= 1e9 # set diagonal values to almost Inf
 
 # Parameters for calculating demand (diurnal function)
@@ -68,8 +74,13 @@ my_shift = 0 * 60    # in units of minutes, set to 0 for no shift
 
 # Parameters for calculating average space-mean speed, and therefore fluxes
 my_λ = 1
+<<<<<<< HEAD
+my_a = 50
+v_f_base = 30
+=======
 my_a = 20
 v_f_base = 48.28
+>>>>>>> 6733eb0a8aff0380423a1d8620d73528cb191cf1
 my_v_f = v_f_base / 60 * ones(Np, Np, Nc)  # Divided by 60 so that this is km per min
 my_v_f[[CartesianIndex(i, i, k) for i in 1:Np, k in 1:Nc]] .= 0 # no movement in loops
 println("Free-flow velocities (my_v_f):")
@@ -81,7 +92,7 @@ p = [v_f => my_v_f, a => my_a, λ => my_λ,
 ];
 
 # Simplify structure and set up model
-println("Simplifying structure...") # whatever that means?
+println("Simplifying structure...") # because the system has vector unknowns, we need to simplify it
 simple_sys = structural_simplify(ode)
 
 println("Creating problem...")
@@ -104,6 +115,7 @@ Calculate average speeds
     plotting functions...
 =#
 
+#=
 println("Calculate average space-mean speeds...")
 N = length(sol)
 long_kc_half_jam = [my_kc_half_jam for i in 1:N]
@@ -112,6 +124,8 @@ avg_v = avg_speed.(long_v_f, my_a, sol[kc], long_kc_half_jam)
 
 println("Speeds")
 display(avg_v)
+
+=#
 
 println("Plotting results...")
 traffic_plots.plot_results(sol)
