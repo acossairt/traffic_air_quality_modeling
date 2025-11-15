195ad7da6eb511cb4c59d8ba938dd2b55cfc7727
John <m.anderies@asu.edu>
2025-11-14 23:02:48 -0700

    compact version of code runs.  Identical to your code except I have done a bit more vectorizing.

--- julia_model/traffic_v2_compact.jl
+++ julia_model/traffic_v2_compact.jl
@@ -12,13 +12,13 @@ NumCors = 1
 @variables u(t) v(t)
 
 # parameters
-@parameters dsharp = 100 dur = 0.97 shift1 = 0 shift2 = 0
+@parameters dsharp = 100 dur = 0.97 shift1 = 1 shift2 = -2
 @parameters demhf12 = 3000 demhf21 = 500 bgd1 = 0 bgd2 = 0
 @parameters ψ =100 L = 30
 @parameters vff[1:NumPatches,1:NumPatches,1:NumCors] 
 @parameters nc_half_ff[1:NumPatches,1:NumPatches,1:NumCors] 
 @parameters vsharp[1:NumPatches,1:NumPatches,1:NumCors]
-@parameters onff on_half onsharp
+@parameters onff = 5000 on_half = 0.5 onsharp = 1
 @parameters period = 24
 
 # trip demand and corridor velocity helper functions
@@ -39,7 +39,7 @@ demand_eqs = [
     γ[1,1] ~ 0,
     γ[1,2] ~ (1 .- g(np[1], 1, demhf12, 4)) .* f(v_shifted(shift1), dsharp, dur, 1) .+ bgd1,
     γ[2,1] ~ (1 .- g(np[2], 1, demhf21, 4)) .* f(-v_shifted(shift2), dsharp, dur, 1) .+ bgd2,
-    γ[1,1] ~ 0
+    γ[2,2] ~ 0
 ]
 
 # velocity expressions: don't collect; keep as ArrayOps
@@ -57,7 +57,6 @@ PatchExitFlux  = [sum(collect(CorEntryFlux[i, :, :])) for i in 1:NumPatches]
 PatchEntryFlux = [sum(collect(CorExitFlux[:, i, :])) for i in 1:NumPatches] 
 
 # now build dynamics using expressions directly
-
 eqs = [
 D.(np) ~ PatchEntryFlux .- PatchExitFlux,
 D.(nc) ~ CorEntryFlux .- CorExitFlux,
@@ -65,5 +64,32 @@ demand_eqs...,
 clock_eqs...
 ]
 
+# Assemble the ODE system
 @named ode = ODESystem(eqs, t)
 
+# Assemble parameters and initial conditions
+p = merge(
+    ModelingToolkit.get_defaults(ode),
+    Dict(vff[i,j,k] => (i==j ? 0.0 : 120.0) for i in 1:NumPatches, j in 1:NumPatches, k in 1:NumCors),
+    Dict(nc_half_ff[i,j,k] => (i==j ? 0.0 : 0.3) for i in 1:NumPatches, j in 1:NumPatches, k in 1:NumCors),
+    Dict(vsharp[i,j,k] => (i==j ? 0.0 : 1.0) for i in 1:NumPatches, j in 1:NumPatches, k in 1:NumCors)
+)
+
+u0 = [
+    np[1] => 10000,
+    np[2] => 0,
+    nc => zeros(NumPatches, NumPatches, NumCors),
+    u => 1.0,
+    v => 0.0
+]
+
+tspan = (0.0, 48.0)
+
+# Create the ODE problem
+prob = ODEProblem(mtkcompile(ode), merge(Dict(u0),p), tspan)
+
+# Solve the ODE problem
+sol = solve(prob, Tsit5(), saveat=0.1)
+
+# Plot results
+plot(sol, idxs=[np[1], np[2], nc[1,2,1], nc[2,1,1]], xlabel="Time (hours)", ylabel="Population", title="Patch Populations Over Time")
